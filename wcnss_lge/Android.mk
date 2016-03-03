6452117e189ef699c9e29980b289622a19e8c57a
fefifofum <jorsellis@gmail.com>
2016-03-03 03:43:59 +0100

    Rename awifi -> v500

--- wcnss_lge/Android.mk
+++ wcnss_lge/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_DEVICE),awifi)
+ifeq ($(TARGET_DEVICE),v500)
 
 LOCAL_PATH:= $(call my-dir)
 
